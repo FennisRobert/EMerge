a99c9ef07b3713ee81a4dbe3ce61132f8ea68a02
EMerge <emerge@MacBookPro.home>
2025-06-09 20:05:19 +0200

    Added license headers

--- fem/elements/femdata.py
+++ fem/elements/femdata.py
@@ -1,11 +1,26 @@
+# EMerge is an open source Python based FEM EM simulation module.
+# Copyright (C) 2025  Robert Fennis.
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see
+# <https://www.gnu.org/licenses/>.
+
 from __future__ import annotations
 from ..mesh3d import Mesh3D
 import numpy as np
-from ..dataset_old import Axis, Dataset
 from typing import Callable
 from scipy.sparse import coo_matrix, csr_matrix
-#from ..bc import BoundaryCondition
-#from typing import Callable
+
 
 
 class FEMBasis:
